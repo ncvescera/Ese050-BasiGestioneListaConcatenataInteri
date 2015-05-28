c0084190ea6fa6dba7dfc665898b7335019e34f6
Nicolò Vescera <slayercandy@icloud.com>
2015-05-28 15:47:02 +0200

    Update main.c

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -7,13 +7,13 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
 CND_ARTIFACT_NAME_Debug=ese050-basigestionelistaconcatenatainteri
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/ese050-basigestionelistaconcatenatainteri
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/ese050-basigestionelistaconcatenatainteri
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
 CND_PACKAGE_NAME_Debug=ese050-basigestionelistaconcatenatainteri.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/ese050-basigestionelistaconcatenatainteri.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/ese050-basigestionelistaconcatenatainteri.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
